0b88265ecc0a82d03bb522ded7341629075fb86b
mas2000 <118755651+mas2000@users.noreply.github.com>
2022-11-23 15:17:37 -0300

    Add files via upload

---
+++ app/static/js/layout.js
@@ -0,0 +1,22 @@
+let subMenu = document.getElementById("subMenu");
+
+function toggleMenu()
+{
+    subMenu.classList.toggle("open-menu");
+}
+
+document.addEventListener("DOMContentLoaded", function (event) {
+  //código a ejecutar cuando el DOM está listo para recibir acciones
+  var alertElem
+  alertElem = document.getElementById('alerts')
+  if (typeof (alertElem) != 'undefined' && alertElem != null) {
+    alertElem.style.display = 'block';
+    alertElem.classList.remove('height');
+    setTimeout(() => {
+      alertElem.style.display = 'none';
+  }, 3000);
+
+  }
+  
+
+});
